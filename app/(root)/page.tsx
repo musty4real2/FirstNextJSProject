9dbd0b01f4feebe5a8e65a641084bdec015fb360
Mustapha Suleiman <mustapha.suleiman@veristein.com>
2025-02-06 00:53:41 +0100

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hello from "@/app/components/hello";
 export default function Home() {
   // console.log("What am i doing here ? -- SERVER/CLIENT");
-    throw new Error("NOT IMPLEMENTED");
+  //   throw new Error("NOT IMPLEMENTED");
   return (
       <>
       <h1 className="text-3xl">Welcome to NextJS.</h1>
