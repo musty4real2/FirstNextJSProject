50f97350baf416d1884acc6b3a0d5e35ef4d3c47
Mustapha Suleiman <mustapha.suleiman@veristein.com>
2025-02-05 14:28:02 +0100

    first commit

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Hello(){
+    console.log("i am a Client Component");
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+export default Hello;
